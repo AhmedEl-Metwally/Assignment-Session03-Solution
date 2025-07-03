734085ad99972c2b592cd4325b5a0e54488a5758
AhmedEl-Metwally <ahmed.moh.elmetwally@gmail.com>
2025-07-03 03:00:42 +0300

    Add project files.

---
+++ Assignment Session03/Baseclass.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment_Session03
+{
+    public class Baseclass
+    {
+        public NextBaseclass nextNumber = new NextBaseclass();
+        public int number;
+        public int numberOne;
+        public decimal numberTwo;
+        public int numberThree;
+        public int length;
+        public decimal principalAmount;
+        public decimal rateOfInterest;
+        public int numberOfMonths;
+        public decimal weight;
+        public decimal height;
+        public int day;
+        public int month;
+        public int year;
+        public string First = string.Empty;
+        public string text = string.Empty;
+        public string Second = string.Empty;
+        public char character ;
+    
+
+    }
+}
+
+
